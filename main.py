fb8286ba09994777a4a5c234bd4cf4aa33ec225d
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-05-21 20:09:28 +0530

    Background remover initial

---
+++ main.py
@@ -0,0 +1,8 @@
+from rembg import remove
+from PIL import Image
+
+input_path = input("Enter image path: ")
+output_path =input("Enter output file name: ")
+inpu = Image.open(input_path)
+output = remove(inpu)
+output.save(output_path+'.png')
