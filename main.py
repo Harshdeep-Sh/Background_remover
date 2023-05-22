c71b6f8c84edf286aa9e1a4f8931006dd3d0850d
Harshdeep Shrivastava <80636729+Harshdeep-Sh@users.noreply.github.com>
2023-05-22 21:25:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from rembg import remove
 from PIL import Image
 
 input_path = input("Enter image path: ")
-output_path =input("Enter output file name: ")
+output_path =input("Enter output file name(without extension): ")
 inpu = Image.open(input_path)
 output = remove(inpu)
 output.save(output_path+'.png')
